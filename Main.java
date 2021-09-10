307ffae2a9db7b5f4487e5423b5864ef1bdf7b53
magink <magink1991@gmail.com>
2021-09-10 22:58:45 +0200

    splits by regex

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-
-
+import com.github.magink.tokenizer.Tokenizer;
 
 public class Main {
   public static void main(String[] args) {
-    Tokenizer tokenizer = new Tokenizer("Hejsan lillebror");
-    int currentToken = tokenizer.getCurrentToken();
-    System.out.println(currentToken);
+    String regexInteger = "[0-9]+";
+    String regexFloat = "[0-9]+\\.[0-9]+";
+    // String regexWord = "/[\\w|åäöÅÄÖ]+/g"; 
+    String regexWord = "[\\w]+"; 
+    String englishWords = "This was the day when... it happened";
+    String swedishWords = "Det var en gång en liten hund.";
+    String numbers = "1265 432534 234 5";
+    String[] manyRegex = new String[] {regexWord, regexInteger};
+
+    Tokenizer tokenizerWord = new Tokenizer(regexInteger, numbers);
   }
 }
--- com/github/magink/tokenizer/Tokenizer.java
+++ com/github/magink/tokenizer/Tokenizer.java
@@ -1,14 +1,51 @@
 package com.github.magink.tokenizer;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Tokenizer {
-  private static final String WORD_TOKEN = "/^[0-9]+/ ";
-  private int currentToken = 0;
   private String input = "";
-  Tokenizer(String input) {
+  private int activeToken = 0;
+  private ArrayList<Pattern> patterns = new ArrayList<>();
+  private ArrayList<String> tokens = new ArrayList<>();
+
+  public Tokenizer(String regex, String input) {
     this.input = input;
+    Pattern pattern = Pattern.compile(regex);
+    patterns.add(pattern);
+    // System.out.println("Pattern is " + patterns.toString());
+    findToken();
   }
-  public int getCurrentToken() {
-    System.out.println("currentToken is" + 0);
-    return currentToken;
+  // public Tokenizer(String[] regexes, String input) {
+  //   this.input = input;
+  //   for(String regex : regexes) {
+  //     Pattern pattern = Pattern.compile(regex);
+  //     patterns.add(pattern);
+  //   }
+  //   System.out.println(patterns.toString());
+  // }
+  // public String getToken() {
+  //   return tokens.get(activeToken);
+  // }
+  // public void nextToken() {
+  //   activeToken++;
+  // }
+  private void findToken () {
+    for(int i = 0; i < patterns.size(); i++) {
+      Matcher matcher = patterns
+                        .get(i)
+                        .matcher(input);
+      // System.out.println(matcher.toString());
+      while(matcher.find()) {
+        String token = input
+                      .substring(
+                          matcher.start(),
+                          matcher.end());
+        System.out.println("token value: " + token);
+      }
+      // System.out.println(matcher.toString());
+      tokens.add("a");
+    }
   }
 }
