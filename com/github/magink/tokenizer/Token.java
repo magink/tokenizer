1f3266b923b89c458ecde1c7b50f436c92572abc
magink <magink1991@gmail.com>
2021-09-11 10:58:43 +0200

    Token class, value and type

---
+++ com/github/magink/tokenizer/Token.java
@@ -0,0 +1,16 @@
+package com.github.magink.tokenizer;
+
+class Token {
+  private String type;
+  private String value;
+  Token(String type, String value) {
+    this.type = type;
+    this.value = value;
+  }
+  public String getType() {
+    return type;
+  }
+  public String getValue() {
+    return value;
+  }
+}
