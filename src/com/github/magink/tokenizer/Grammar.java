7b58b960c58275808e825e172d84ce447973055e
magink <magink1991@gmail.com>
2021-09-24 17:27:31 +0200

    refactor: Grammar deals with Patterns and Matcher, not TokenType

--- src/com/github/magink/tokenizer/Grammar.java
+++ src/com/github/magink/tokenizer/Grammar.java
@@ -1,6 +1,8 @@
 package com.github.magink.tokenizer;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A Grammar is made of one or more types. 
@@ -9,6 +11,8 @@ import java.util.ArrayList;
  */
 public class Grammar {
   private ArrayList<TokenType> types;
+  private Pattern pattern;
+  private Matcher matcher;
 
   public Grammar(){
     types = new ArrayList<>();
@@ -18,11 +22,30 @@ public class Grammar {
     if(!types.contains(tokenType)) {
       types.add(tokenType);
     }
+    makePattern();
   }
   public int getNumberOfTokenTypes() {
     return types.size();
   } 
   public TokenType getTokenType(int index) {
     return types.get(index);
+  }
+  public Matcher getMatcher(String toMatch) {
+    if (matcher == null) {
+      matcher = pattern.matcher(toMatch);
+    }
+    return matcher;
+  }
+  private void makePattern() {
+    StringBuilder regexes = new StringBuilder();
+    for(int i = 0; i < getNumberOfTokenTypes(); i++) {
+      if(i > 0) {
+        regexes.append("|");
+      }
+      String token = String.format("(?<%s>%s)", getTokenType(i).getName(), getTokenType(i).getRegex());
+      regexes.append(token);
+    }
+    regexes.append("|\\S");
+    pattern = Pattern.compile(regexes.toString());
   }
 }
--- src/com/github/magink/tokenizer/TokenType.java
+++ src/com/github/magink/tokenizer/TokenType.java
@@ -1,12 +1,8 @@
 package com.github.magink.tokenizer;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class TokenType {
   private String name;
   private String regex;
-  private Matcher matcher;
   protected TokenType(String regex, String name) {
     this.name = name;
     this.regex = regex;
@@ -16,11 +12,5 @@ public class TokenType {
   }
   public String getName() {
     return name;
-  }
-  public Matcher getMatcher(String toMatch) {
-    if (matcher == null) {
-      matcher = Pattern.compile(regex).matcher(toMatch);
-    }
-    return matcher;
   }
 }
