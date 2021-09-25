5fe8c10145f19b37cafea995124fa8a712e11238
magink <magink1991@gmail.com>
2021-09-26 01:16:31 +0200

    test: maximalMunch

--- test/com/github/magink/tokenizer/TestOtherGrammar.java
+++ test/com/github/magink/tokenizer/TestOtherGrammar.java
@@ -11,10 +11,14 @@ public class TestOtherGrammar {
   private static final String WORD = "WORD";
   private static final String COMMA = "COMMA";
   private static final String DOT = "DOT";
+  private static final String FLOAT = "FLOAT";
+  private static final String INTEGER = "INTEGER";
   String regexWord = "[[a-zA-Z]|åäöÅÄÖ]+";
   String regexExclamation = "[!]"; 
   String regexComma = "[,]";
   String regexDot = "[.]";
+  String regexFloat = "[0-9]+(\\.([0-9])+)";
+  String regexInteger = "[0-9]+";
   
   Grammar grammar;
 
@@ -85,6 +89,19 @@ public class TestOtherGrammar {
     grammar.addType(regexWord, "WORD");
     grammar.addType(regexWord, "WORD");
     assertEquals(1, grammar.getNumberOfTokenTypes());
+  }
+  @Test
+  @DisplayName("TC 22") 
+  void maximalMunch() throws LexicalException {
+    String input = "3.14 5";
+    grammar.addType(regexFloat, FLOAT);
+    grammar.addType(regexInteger, INTEGER);
+    Tokenizer tokenizer = new Tokenizer(grammar, input);
+    assertEquals("3.14", tokenizer.getActiveTokenValue());
+    assertEquals(FLOAT, tokenizer.getActiveTokenType());
+    tokenizer.nextToken();
+    assertEquals("5", tokenizer.getActiveTokenValue());
+    assertEquals(INTEGER, tokenizer.getActiveTokenType());
   }
   
   
